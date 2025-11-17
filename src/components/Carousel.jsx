d55e525671e7cfbccfa227009e1d7ced73e679e6
saisriya177 <saisriyaambati177@gmail.com>
2025-11-17 14:29:25 +0530

    updated UI and fixed issues

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -58,7 +58,7 @@ const Carousel = () => {
                                     <h1 className='md:text-4xl text-xl font-bold uppercase line-clamp-1 md:line-clamp-3 md:w-[500px] text-white'>{item.title}</h1>
                                     <p className='md:w-[500px] line-clamp-3 text-gray-400 pr-7'>{item.description}</p>
                                     <button
-                                    onClick={()=>{navigate()}}
+                                    onClick={()=>{navigate("/products")}}
                                     className='bg-gradient-to-r from-red-500 to-purple-500 text-white px-3 py-2 rounded-md cursor-pointer mt-2'>Shop Now</button>
                                 </div>
                                 <div>
